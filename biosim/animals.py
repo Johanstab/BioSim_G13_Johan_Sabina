4d6c53fbbd7c0c816e62270cb98a259af6f3a39f
Sabina <sababina@hotmail.com>
2020-06-04 20:49:30 +0200

    Fixed the way the deafult herbvore values are presented

--- biosim/animals.py
+++ biosim/animals.py
@@ -63,25 +63,25 @@ class Animals:
 
 
 class Herbivore(Animals):
-    def __init__(self, age, weight):
+
+    w_birth = 8.0
+    sigma_birth = 1.5
+    beta = 0.9
+    eta = 0.05
+    a_half = 40.0
+    phi_age = 0.6
+    w_half = 10.0
+    phi_weight = 0.1
+    mu = 0.25
+    gamma = 0.2
+    zeta = 3.5
+    xi = 1.2
+    omega = 0.4
+    F = 10.0
+
+    def __init__(self, age=0, weight=None):
         super().__init__(age, weight)
 
-        default_params_herbivore = {
-            'w_birth': 8.,
-            'sigma_birth': 1.5,
-            'beta': 0.9,
-            'eta': 0.05,
-            'a_half': 40.0,
-            'phi_age': 0.6,
-            'w_half': 10.0,
-            'phi_weight': 0.1,
-            'mu': 0.25,
-            'gamma': 0.2,
-            'zeta': 3.5,
-            'xi': 1.2,
-            'omega': 0.4,
-            'F': 10.0,
-            'DeltaPhiMax': None}
 
 
 class Carnivore(Animals):
