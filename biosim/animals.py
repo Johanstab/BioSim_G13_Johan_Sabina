4825a27d010d807a5808387066d996f5d36520e2
Johan Stabekk <johan.stabekk@nmbu.no>
2020-06-04 12:30:24 +0200

    Defined default animal parameters

--- biosim/animals.py
+++ biosim/animals.py
@@ -5,7 +5,24 @@ __email__ = 'johansta@nmbu.no, sabinal@nmbu.no'
 
 
 class Animals:
-    def __init__(self):
+    default_params = {
+        'w_birth': 8.,
+        'sigma_birth': 1.5,
+        'beta': 0.9,
+        'eta': 0.05,
+        'a_half': 40.,
+        'phi_age': 0.2,
+        'w_half': 10.,
+        'phi_weight': 0.1,
+        'mu': 0.25,
+        'gamma': 0.2,
+        'zeta': 3.5,
+        'xi': 1.2,
+        'omega': 0.4,
+        'F': 10.
+    }
+
+    def __init__(self, params=default_params):
         pass
 
 
