f32dfa097aca7f479412c241dabc296407bca053
Johan Stabekk <johan.stabekk@nmbu.no>
2020-06-05 11:52:40 +0200

    Added cycle description.

--- biosim/cycle.py
+++ biosim/cycle.py
@@ -5,6 +5,16 @@ __email__ = 'johansta@nmbu.no, sabinal@nmbu.no'
 
 
 class Cycle:
+    """
+    Defines the order of what happens in a year:
+    1. Food grows and they feed (Herbivores then carnivores)
+    2. They procreate based on probability and fitness
+    3. They migrate (Will wait with this)
+    4. They age
+    5. They loose weight according to parameters
+    6. Some die based on probability and fitness
+
+    """
     def __init__(self):
         pass
 
