8aaab4df42c68eef602eca1bb2f5607dad3a4ca2
Sabina <sababina@hotmail.com>
2020-06-05 10:39:47 +0200

    Changed params

--- biosim/animals.py
+++ biosim/animals.py
@@ -66,23 +66,26 @@ class Animals:
 
 class Herbivore(Animals):
 
-    w_birth = 8.0
-    sigma_birth = 1.5
-    beta = 0.9
-    eta = 0.05
-    a_half = 40.0
-    phi_age = 0.6
-    w_half = 10.0
-    phi_weight = 0.1
-    mu = 0.25
-    gamma = 0.2
-    zeta = 3.5
-    xi = 1.2
-    omega = 0.4
-    F = 10.0
+    params_dic_Herbivore = {'w_birth': 8.,
+              'sigma_birth': 1.5,
+              'beta': 0.9,
+              'eta': 0.05,
+              'a_half': 40.,
+              'phi_age': 0.6,
+              'w_half': 10.,
+              'phi_weight': 0.1,
+              'mu': 0.25,
+              'gamma': 0.2,
+              'zeta': 3.5,
+              'xi': 1.2,
+              'omega': 0.4,
+              'F': 10.}
 
     def __init__(self, age=0, weight=None):
         super().__init__(age, weight)
+
+    def feed_Herbivore(self, cell):
+
 
 
 class Carnivore(Animals):
