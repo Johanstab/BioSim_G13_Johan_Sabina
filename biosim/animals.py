2506bb736feac577b859e9325baf0ca305c454f9
Sabina <sababina@hotmail.com>
2020-06-05 12:26:10 +0200

    Added things to to birth function

--- biosim/animals.py
+++ biosim/animals.py
@@ -76,19 +76,19 @@ class Animals:
 
 class Herbivore(Animals):
     params = {'w_birth': 8.,
-                   'sigma_birth': 1.5,
-                   'beta': 0.9,
-                   'eta': 0.05,
-                   'a_half': 40.,
-                   'phi_age': 0.6,
-                   'w_half': 10.,
-                   'phi_weight': 0.1,
-                   'mu': 0.25,
-                   'gamma': 0.2,
-                   'zeta': 3.5,
-                   'xi': 1.2,
-                   'omega': 0.4,
-                   'F': 10.}
+              'sigma_birth': 1.5,
+              'beta': 0.9,
+              'eta': 0.05,
+              'a_half': 40.,
+              'phi_age': 0.6,
+              'w_half': 10.,
+              'phi_weight': 0.1,
+              'mu': 0.25,
+              'gamma': 0.2,
+              'zeta': 3.5,
+              'xi': 1.2,
+              'omega': 0.4,
+              'F': 10.}
 
     def __init__(self, age=0, weight=None):
         super().__init__(age, weight)
@@ -101,8 +101,16 @@ class Herbivore(Animals):
             self.weight = self.weight_gain(self.params['beta'], cell.availabe_fodder)
             self.weight = self.weight_loss(self.params['beta'], self.weight)
 
-    def birth(self):
-        pass
+    def birth(self, nr_animals):
+        b_prob = min(1, self.params['gamma'] *
+                     self.fitness * (nr_animals - 1))
+        if nr_animals < 2:
+            return False
+        if self.weight < self.params['zeta'] * (
+                self.params['w_birth'] + self.params['sigma_birth']):
+            return False
+        if random.random() < b_prob:
+            return True
 
 
 class Carnivore(Animals):
