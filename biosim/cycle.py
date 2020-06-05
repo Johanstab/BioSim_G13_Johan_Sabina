c12c77e3e1dfc7854ac0768f40a6748f838034fd
Johan Stabekk <johan.stabekk@nmbu.no>
2020-06-05 11:36:32 +0200

    Created a new file for the annual cycle on Rossumøya.

---
+++ biosim/cycle.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Johan Stabekk, Sabina Langås'
+__email__ = 'johansta@nmbu.no, sabinal@nmbu.no'
+
+
+class Cycle:
+    def __init__(self):
+        pass
+
+    def feeding(self):
+        pass
+
+    def procreation(self):
+        pass
+
+    def migration(self):
+        pass
+
+    def aging(self):
+        pass
+
+    def weight_loss(self):
+        pass
+
+    def death(self):
+        pass
