3bae94d372613333ad4a54e72a6ac39f8db87fd7
Sabina <sababina@hotmail.com>
2020-06-07 15:28:25 +0200

    Added new test that actually works

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -7,6 +7,18 @@ import pytest
 from biosim.animals import Herbivore, Carnivore
 from biosim.landscapes import Landscape
 import biosim.island
+import random
 
 
+def test_init():
+    """
+    Test that the init method works for both carnivores and herbivores.
+    """
+    carn = Carnivore(3, 40)
+    herb = Herbivore(2, 27)
 
+    assert carn.weight == 40
+    assert carn.age == 3
+
+    assert herb.weight == 27
+    assert herb.age == 2
