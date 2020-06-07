090ca54c28abb99883ef614de371fbd56fccb51e
Sabina <sababina@hotmail.com>
2020-06-07 15:17:35 +0200

    Removed test

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -5,28 +5,8 @@ __email__ = 'johansta@nmbu.no, sabinal@nmbu.no'
 
 import pytest
 from biosim.animals import Herbivore, Carnivore
-from biosim.landscapes import Lowland
+from biosim.landscapes import Landscape
 import biosim.island
 
 
-class TestAnimal:
 
-    @pytest.fixture()
-    def create_herb(self):
-        """Creates a Herbivore object"""
-        herb = Herbivore(5, 10)
-        return herb
-
-    def herb_eat_and_gain_weight(self, create_herb):
-        """Herbivores weight should increase when eating"""
-        prev_weigth = create_herb.eats(Lowland)
-
-    def herb_loses_weight(self):
-        pass
-
-    def herb_aging(self):
-        pass
-
-
-    def herb_age_zero_birth(self):
-        pass
