8786b863cd5d45a3246f829858577bfc8b2f8182
Sabina <sababina@hotmail.com>
2020-06-07 18:26:54 +0200

    Added even more tests, and descriptions on the tests

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -36,6 +36,9 @@ def test_ageing():
 
 
 def test_weight_loss():
+    """
+    Test that the animals loses weight for every year
+    """
     herbivore = Herbivore(9, 30)
     herbivore.weight_loss()
 
@@ -44,17 +47,52 @@ def test_weight_loss():
 
 
 def test_weight_gain():
-    herbivore = Herbivore(15, 20)
+    """
+    Test that the animals gains the right amount of weight after eating"
+    """
+    cell = Landscape()
+    initial_pop = [{'species': 'Herbivore', 'age': 1, 'weight': 20}]
+    cell.set_population(initial_pop)
+    cell.f_max = 800
+    cell.food_grows()
+    cell.animals_eat()
 
-    assert not herbivore.weight == 20
-    assert herbivore.weight == 29
+    assert cell.animal_list[0].weight != 20
+    assert cell.animal_list[0].weight == 29
+
+
+def test_fitness():
+    """
+    Testing that the fitness function works, that the fitness chances when animal aging
+    """
+    herbivore = Herbivore(5, 10)
+    fitness_before = herbivore.fitness
+
+    herbivore.aging()
+
+    fitness_after = herbivore.fitness
+
+    assert fitness_before != fitness_after
 
 
 def test_death():
+    """
+    Testing that the animal dies if the weight is zero
+    """
+
+    herbivore = Herbivore(3, 0)
+
+    assert herbivore.death() is True
 
 
+def test_birth():
+    """
+    Test that the animals can't reproduce if only one animal is present"
+    """
+    cell = Landscape()
 
+    initial_pop = [{'species': 'Herbivore', 'age': 1, 'weight': 10.}]
 
+    cell.set_population(initial_pop)
 
-
-
+    assert cell.animals_reproduce() is False
