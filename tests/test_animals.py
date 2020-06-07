68900ee87aa3d9751a639b48b8ec5f430ab65732
Sabina <sababina@hotmail.com>
2020-06-07 15:05:05 +0200

    Added test

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -12,24 +12,20 @@ import biosim.island
 class TestAnimal:
 
     @pytest.fixture()
-    def creat_herb(self):
+    def create_herb(self):
         """Creates a Herbivore object"""
         herb = Herbivore(5, 10)
         return herb
 
-    def herb_eat_and_gain_weight(self):
-        pass
+    def herb_eat_and_gain_weight(self, create_herb):
+        """Herbivores weight should increase when eating"""
+        prev_weigth = create_herb.eats(Lowland)
 
     def herb_loses_weight(self):
         pass
 
     def herb_aging(self):
         pass
-
-    def test_herb_not_dead(self,create_herb):
-        """ New animal should not be dead"""
-
-        assert create_herb.is_dead is False, "Animal should not be dead"
 
 
     def herb_age_zero_birth(self):
