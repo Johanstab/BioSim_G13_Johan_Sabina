35ee3dbb422038443b7d5bcd0007d5c20eb10baf
Sabina <sababina@hotmail.com>
2020-06-17 20:03:15 +0200

    Added some inline code

--- biosim/visualization.py
+++ biosim/visualization.py
@@ -79,12 +79,15 @@ class Visualization:
         year : int
             the start year of the simulation graphics.
         """
+
+        # create new figure window
         if self._fig is None:
             self._fig = plt.figure(constrained_layout=True, figsize=(8, 6))
             gs = self._fig.add_gridspec(5, 12)
             self._fig.tight_layout()
             plt.axis('off')
 
+        # Add subplot for map
         if self._map_ax is None:
             self._map_ax = self._fig.add_subplot(gs[:2, 0:5])
             self._img_axis = None
@@ -92,6 +95,7 @@ class Visualization:
             self._map_ax.set_xticklabels([])
             self._map_ax.title.set_text('Island')
 
+        # Add subplot for year count
         if self._year_ax is None:
             self._year_ax = self._fig.add_subplot(gs[:2, 5:7])
             self._text = self._year_ax.text(0.5, 0.5, f'Year: {year}',
@@ -101,6 +105,7 @@ class Visualization:
                                             fontsize=14)
             self._year_ax.axis('off')
 
+        # Add subplot for animal count graph
         if self._mean_ax is None:
             self._mean_ax = self._fig.add_subplot(gs[:2, 7:])
             self._mean_ax.set_ylim(0, y_lim)
@@ -111,6 +116,7 @@ class Visualization:
         elif self._mean_ax is not None:
             self._mean_ax.set_xlim(0, x_lim)
 
+        # Add subplot for herbivore distribution
         if self._herb_ax is None:
             self._herb_ax = self._fig.add_subplot(gs[2:4, :6])
             self._herb_axis = None
@@ -118,6 +124,7 @@ class Visualization:
             self._herb_ax.set_xticklabels([])
             self._herb_ax.title.set_text('Herbivore distribution')
 
+        # Add subplot for carnivore distribution
         if self._carn_ax is None:
             self._carn_ax = self._fig.add_subplot(gs[2:4, 6:])
             self._carn_axis = None
@@ -125,18 +132,22 @@ class Visualization:
             self._carn_ax.set_xticklabels([])
             self._carn_ax.title.set_text('Carnivore distribution')
 
+        # Add subplot for fitness histogram
         if self._fitness_axis is None:
             self._fitness_axis = self._fig.add_subplot(gs[4:, :4])
             self._fitness_axis.set_title('Fitness')
 
+        # Add subplot for age histogram
         if self._age_axis is None:
             self._age_axis = self._fig.add_subplot(gs[4:, 4:8])
             self._age_axis.set_title('Age')
 
+        # Add subplot for weight histogram
         if self._weight_axis is None:
             self._weight_axis = self._fig.add_subplot(gs[4:, 8:])
             self._weight_axis.set_title('Weight')
 
+        # Initiate total Herbivores graph
         if self._herb_line is None:
             herb_plot = self._mean_ax.plot(np.arange(0, x_lim),
                                            np.full(x_lim, np.nan),
@@ -144,7 +155,6 @@ class Visualization:
             self._herb_line = herb_plot[0]
         elif self._herb_line is not None:
             self._final_step = x_lim
-
             xdata, ydata = self._herb_line.get_data()
             x_new = np.arange(xdata[-1] + 1, self._final_step)
             if len(x_new) > 0:
@@ -152,6 +162,7 @@ class Visualization:
                 self._herb_line.set_data(np.hstack((xdata, x_new)),
                                          np.hstack((ydata, y_new)))
 
+        # Initiate total Carnivores graph
         if self._carn_line is None:
             carn_plot = self._mean_ax.plot(np.arange(0, x_lim),
                                            np.full(x_lim, np.nan),
@@ -276,6 +287,15 @@ class Visualization:
         self._text.set_text(f'Year:{island_year}')
 
     def update_fitness(self, data_1, data_2):
+        """Updates the fitness histogram of the different spices on the island.
+
+        Parameters
+        ----------
+        data_1 : dict
+                Dict that contains information about fitness for all herbivores present on island.
+        data_2 : dict
+                Dict that contains information about fitness for all carnivores present on island.
+        """
         if self._fitness_hist is None:
             n = np.ceil((self.hist_dict['fitness']['max'] - 0) / self.hist_dict['fitness']['delta'])
             self._fitness_axis.clear()
@@ -288,6 +308,15 @@ class Visualization:
                                     histtype='step', color='r')
 
     def update_age(self, data_1, data_2):
+        """Updates the age histogram of the different spices on the island.
+
+        Parameters
+        ----------
+        data_1 : dict
+                Dict that contains information about age for all herbivores present on island.
+        data_2 : dict
+                Dict that contains information about age for all carnivores present on island.
+        """
         if self._age_hist is None:
             n = np.ceil((self.hist_dict['age']['max'] - 0) / self.hist_dict['age']['delta'])
             self._age_axis.clear()
@@ -300,6 +329,15 @@ class Visualization:
                                 histtype='step', color='r')
 
     def update_weight(self, data_1, data_2):
+        """Updates the weight histogram of the different spices on the island.
+
+        Parameters
+        ----------
+        data_1 : dict
+                Dict that contains information about weight for all herbivores present on island.
+        data_2 : dict
+                Dict that contains information about weight for all carnivores present on island.
+        """
         if self._weight_hist is None:
             n = np.ceil((self.hist_dict['weight']['max'] - 0) / self.hist_dict['weight']['delta'])
             self._weight_axis.clear()
