83c2055d62421583b3583e5ed0f74faa37cf7edb
Sabina <sababina@hotmail.com>
2020-06-17 19:34:17 +0200

    Made more docstrings

--- tests/test_landscapes.py
+++ tests/test_landscapes.py
@@ -10,7 +10,7 @@ from biosim.animals import Herbivore, Carnivore
 
 @pytest.fixture(autouse=True)
 def test_set_animal_parameters():
-    """Sets default parameters to animals."""
+    """Sets default parameters to animals for the tests"""
     Herbivore.set_params({
         "w_birth": 8.0,
         "sigma_birth": 1.5,
@@ -73,6 +73,7 @@ def test_init_landscapes():
 
 
 def test_food_grows_lowland():
+    """Test that fodder will grow in Lowland"""
     l_scape = Lowland()
     l_scape.available_food = 0
     l_scape.food_grows()
@@ -82,6 +83,7 @@ def test_food_grows_lowland():
 
 
 def test_set_params_error():
+    """Test if error is raised when invalid key or value is past/used."""
     new_params1 = {'fodder_max': 10}
     new_params2 = {'f_max': -10}
     with pytest.raises(KeyError):
@@ -91,6 +93,7 @@ def test_set_params_error():
 
 
 def test_set_population():
+    """Test that population can be set in landscapes"""
     init_herb = [{"species": "Herbivore", "age": 1, "weight": 10.0} for _ in range(10)]
 
     init_carn = [{"species": "Carnivore", "age": 3, "weight": 14.0} for _ in range(10)]
@@ -104,6 +107,7 @@ def test_set_population():
 
 
 def test_add_population():
+    """Test if population can be added to landscapes"""
     l_scape = Landscape()
     l_scape.add_population(Herbivore(5, 20))
     l_scape.add_population(Carnivore(5, 20))
@@ -112,6 +116,7 @@ def test_add_population():
 
 
 def test_food_grows_highland():
+    """Test that fodder will grow in Lowland"""
     l_scape = Highland()
     l_scape.available_food = 0
     l_scape.food_grows()
@@ -121,6 +126,7 @@ def test_food_grows_highland():
 
 
 def test_herbivore_eats():
+    """Test that the herbivore eats, by checking if it gains weight or not"""
     ini_pop = [{"species": "Herbivore", "age": 3, "weight": 20.0},
                {"species": "Herbivore", "age": 3, "weight": 20.0}]
     l_scape = Lowland()
@@ -133,6 +139,7 @@ def test_herbivore_eats():
 
 
 def test_herbivore_does_not_eat():
+    """Test that the herbivore do not eat, by checking if its weight stays the same"""
     ini_pop = [{"species": "Herbivore", "age": 3, "weight": 20.0},
                {"species": "Herbivore", "age": 3, "weight": 20.0}]
     l_scape = Lowland()
@@ -144,6 +151,8 @@ def test_herbivore_does_not_eat():
 
 
 def test_herbivore_eats_available_food():
+    """Test that the herbivore only eats whats available of food, by checking the total weight
+    of the present herbivores and that available food becomes 0"""
     ini_pop = [{"species": "Herbivore", "age": 3, "weight": 20.0},
                {"species": "Herbivore", "age": 3, "weight": 20.0}]
     l_scape = Lowland()
@@ -156,6 +165,8 @@ def test_herbivore_eats_available_food():
 
 
 def test_carnivore_eats():
+    """Test that the carnivores eats, by checking if the amount of herbivores are different before
+    and after carnivores eats"""
     ini_pop = [{"species": "Herbivore", "age": 3, "weight": 10.0} for _ in range(10)] + \
               [{"species": "Carnivore", "age": 3, "weight": 14.0} for _ in range(10)]
     l_scape = Desert()
@@ -168,6 +179,7 @@ def test_carnivore_eats():
 
 
 def test_no_reproduce_one_animal():
+    """Test that no reproducing will happen when only on animal for the species is present"""
     ini_pop = [{"species": "Herbivore", "age": 1, "weight": 10.0} for _ in range(1)] + \
               [{"species": "Carnivore", "age": 3, "weight": 14.0} for _ in range(1)]
     l_scape = Lowland()
@@ -177,6 +189,7 @@ def test_no_reproduce_one_animal():
 
 
 def test_reproduce_herbivore(mocker):
+    """Test that the herbivore """
     mocker.patch('numpy.random.random', return_value=0.00001)
     init_pop = [{"species": "Herbivore", "age": 3, "weight": 50.0},
                 {"species": "Herbivore", "age": 3, "weight": 54.0},
